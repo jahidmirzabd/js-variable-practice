f463889e8f29d71e05159f1d07a03bae2d012103
jahidmirzabd <jahidmirzabd@gmail.com>
2024-05-25 19:17:09 +0600

    first commit

---
+++ apps.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 70;
+let ab= a+b;
+
+if (a>b) {
+    document.getElementById("title").innerHTML = "good boss";
+}else{
+    document.getElementById("title").innerHTML = "Not good boss";
+}
